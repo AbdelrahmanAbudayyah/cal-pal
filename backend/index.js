d4939767c06fb2aeac5a6360eba67a95d610b05c
AbdelrahmanAbudayyah <115599944+AbdelrahmanAbudayyah@users.noreply.github.com>
2024-07-11 19:02:28 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,12 @@ const corsOptions = {
    credentials:true,            //access-control-allow-credentials:true
    optionSuccessStatus:200
 };
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 
